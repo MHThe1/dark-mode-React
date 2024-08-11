4e33d6da7ead1af514126061207cd255e0cb1e38
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-08-12 01:07:48 +0600

    best implementation of dark mode yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useLocalStorage from 'use-local-storage';
 
-function App() {
-  const [count, setCount] = useState(0)
+import SunIcon from './components/icons/SunIcon';
+import MoonIcon from './components/icons/MoonIcon';
 
+export default function App() {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [darkMode, setDarkMode] = useLocalStorage("isDark", preference);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={`${darkMode && "dark"}`}>
+      <main className="flex min-h-screen flex-col p-12 bg-neutral-100 dark:bg-neutral-900 ">
+        <nav className="grid grid-cols-3 items-center">
+          <h1 className="text-xl font-semibold dark:text-white ">mhthe1</h1>
+          <span className="justify-self-center text-blue-600 font-mono">
+            Dark Mode Implementation with React and Tailwind
+          </span>
+          <button
+            onClick={toggleDarkMode}
+            className="justify-self-end w-8 h-8 rounded-full flex items-center justify-center"
+          >
+            {darkMode ? (
+              <SunIcon color="white" />
+            ) : (
+              <MoonIcon />
+            )}
+          </button>
+        </nav>
+        <section className="w-[90%] max-w-3xl mx-auto mt-40 flex flex-col gap-8">
+          <h1 className="text-blue-600 text-5xl font-semibold">
+            The journey of a Developer!
+          </h1>
+          <p className="text-sky-800 dark:text-neutral-300 text-2xl tracking-wide leading-relaxed">
+            I am Mehedi Hasan Tanvir, hailing from Dhaka, Bangladesh.
+            I have been the curious kind since my childhood, trying out all sorts of stuff.
+            Currently, I'm delving deep into the world of ReactJs
+          </p>
+          <div className="mt-12 text-neutral-900 dark:text-neutral-200">
+            <h2 className="text-4xl font-bold text-blue-600 mb-6">Skills</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 text-lg">
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-indigo-500">•</span> HTML
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-green-500">•</span> CSS
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-yellow-500">•</span> JavaScript
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-blue-500">•</span> React
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-purple-500">•</span> Tailwind CSS
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-red-500">•</span> Python
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-indigo-500">•</span> Flask
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-green-500">•</span> Django
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-yellow-500">•</span> C++
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-blue-500">•</span> MySQL
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-purple-500">•</span> DSA
+              </div>
+              <div className="flex items-center">
+                <span className="mr-2 text-2xl text-red-500">•</span> Git
+              </div>
+              
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
-
-export default App
